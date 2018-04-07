c13cfc6c5e4c5f78cff1af4d8b2bad0d018e8a1e
Sheila Nasehi (BDA/Outsourcing/Shared Svcs <sheila.nasehi@towerswatson.com>
2018-04-07 13:39:26 -0700

    new version

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -6,23 +6,23 @@ module.exports = function (app) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
 
-  app.use(function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/home.html'));
-  });
+  // app.use(function (req, res) {
+  //   res.sendFile(path.join(__dirname + '/../public/home.html'));
+  // });
 
   app.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/survey.html'));
   });
 
-  app.use(function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/survey.html'));
-  });
+  // app.use(function (req, res) {
+  //   res.sendFile(path.join(__dirname + '/../public/survey.html'));
+  // });
 
   app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../public/home.html'));
   });
 
-  app.use(function (req, res) {
-    res.sendFile(path.join(__dirname + '/../public/home.html'));
-  });
+  // app.use(function (req, res) {
+  //   res.sendFile(path.join(__dirname + '/../public/home.html'));
+  // });
 }
